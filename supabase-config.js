db24ecf2715d5ffd84ff392a1a03869751a4cb65
mohmdfartka00-collab <mohmdfartka00@gmail.com>
2026-08-29 14:18:33 -0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,3 +2,7 @@ window.FARTAKA_SUPABASE_CONFIG={
   "supabase_url": "https://kycvxqjjdkoatemjxaoz.supabase.co",
   "supabase_publishable_key": "sb_publishable_cbO3YB9h3W-2zKP3sVra9w_Ock3cyC7"
 };
+window.FARTAKA_SUPABASE_CONFIG = {
+  supabase_url: 'https://kycvxqjjdkoatemjxaoz.supabase.co',
+  supabase_publishable_key: 'sb_publishable_cbO3YB9h3W-2zKP3sVra9w_Ock3cyC7'
+};
